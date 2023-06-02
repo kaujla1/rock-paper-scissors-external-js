e295bb9f9f389b187670b8ef2931bc0df54b5ad3
Kiv Aujla <89060170+kaujla1@users.noreply.github.com>
2023-06-02 12:49:00 -0400

    Display score, display round result, display game result

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*generate a random choice for the computer*/      
+/*generate a random choice for the computer*/
 function getComputerChoice() {
   let computerOptions = ["Rock", "Paper", "Scissors"];
   let computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
@@ -16,32 +16,90 @@ function playerSelectionPrompt() {
 /*Connect to body div*/
 const body = document.querySelector('body');
 
+/*Connect to choices div*/
+const choiceDiv = document.querySelector(".choices");
+
 /*Create DOM results div*/
 const resultsDiv = document.createElement("div");
 resultsDiv.classList.add("results");
-body.appendChild(resultsDiv); //CHANGE THIS when you're finishing so that the resultsDiv is appended somewhere else, not at the bottom of body. You might have to connect to another element to use it as a reference point. 
-
+body.insertBefore(resultsDiv, choiceDiv); //CHANGE THIS when you're finishing so that the resultsDiv is appended somewhere else, not at the bottom of body. You might have to connect to another element to use it as a reference point. 
 
 /*play a single round of rock paper scissors and return the result of the round*/
+let result;
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "rock" && computerSelection === "Scissors") {
-    return resultsDiv.textContent = "You win! Rock beats Scissors."; 
+    result = "You win! Rock beats Scissors.";
+    resultsDiv.textContent = result; /*not sure if I need a return before all of these?*/
   } else if (playerSelection === "rock" && computerSelection === "Paper") {
-    return resultsDiv.textContent = "You lose. Paper beats Rock.";
+    result = "You lose. Paper beats Rock.";
+    resultsDiv.textContent = result;
   } else if (playerSelection === "rock" && computerSelection === "Rock") {
-    return resultsDiv.textContent = "It's a tie! You both chose rock.";
+    result = "It's a tie! You both chose rock.";
+    resultsDiv.textContent = result;
   } else if (playerSelection === "paper" && computerSelection === "Rock") {
-    return resultsDiv.textContent = "You win! Paper beats Rock.";
+    result = "You win! Paper beats Rock.";
+    resultsDiv.textContent = result;
   } else if (playerSelection === "paper" && computerSelection === "Scissors") {
-    return resultsDiv.textContent = "You lose. Scissors beat Paper.";
+    result = "You lose. Scissors beat Paper.";
+    resultsDiv.textContent = result;
   } else if (playerSelection === "paper" && computerSelection === "Paper") {
-    return resultsDiv.textContent = "It's a tie! You both chose Paper.";
+    result = "It's a tie! You both chose Paper.";
+    resultsDiv.textContent = result;
   } else if (playerSelection === "scissors" && computerSelection === "Paper") {
-    return resultsDiv.textContent = "You win! Scissors beat Paper.";
+    result = "You win! Scissors beat Paper.";
+    resultsDiv.textContent = result;
   } else if (playerSelection === "scissors" && computerSelection === "Rock") {
-    return resultsDiv.textContent = "You lose. Rock beats Scissors.";
+    result = "You lose. Rock beats Scissors.";
+    resultsDiv.textContent = result;
   } else if (playerSelection === "scissors" && computerSelection === "Scissors") {
-    return resultsDiv.textContent = "It's a tie! You both chose Scissors.";
+    result = "It's a tie! You both chose Scissors.";
+    resultsDiv.textContent = result;
+  }
+  keepScore();
+  calculateGameResult();
+}
+
+/*keep track of players' scores after each round*/
+const scoreboardPlayer = document.querySelector(".player"); 
+
+const playerScore = document.createElement("p");
+playerScore.classList.add("player-score");
+playerScore.textContent = 0;
+scoreboardPlayer.appendChild(playerScore);
+
+const scoreboardComputer = document.querySelector(".computer");
+
+const computerScore = document.createElement("p");
+computerScore.classList.add("computer-score")
+computerScore.textContent = 0; 
+scoreboardComputer.appendChild(computerScore);
+
+function keepScore() {
+  if (result === "You win! Rock beats Scissors." || result === "You win! Paper beats Rock." || result === "You win! Scissors beat Paper.") {
+    playerScore.textContent = Number(playerScore.textContent) + 1;
+  } else if (result === "You lose. Paper beats Rock." || result === "You lose. Scissors beat Paper." || result === "You lose. Rock beats Scissors.") {
+    computerScore.textContent = Number(computerScore.textContent) + 1;
+  }
+}
+
+/*calculate the results after all 5 rounds are finished*/
+const gameResult = document.createElement("p");
+gameResult.classList.add("game-result");
+body.insertBefore(gameResult, resultsDiv); //move this once finished
+
+function calculateGameResult() {
+  if (playerScore.textContent === "5" && computerScore.textContent === "5") {
+    gameResult.textContent = "Game result: it's a tie game!";
+    //remove event listener
+  } else if (playerScore.textContent === "5") {
+    gameResult.textContent = "Game result: you win!";
+    resultsDiv.textContent = "";
+    //remove event listener
+  } else if (computerScore.textContent === "5") {
+    gameResult.textContent = "Game result. you lose.";
+    resultsDiv.textContent = "";
+    //remove event listener
   }
 }
 
@@ -53,6 +111,7 @@ choiceButtons.forEach((button) => {
     playRound(button.className, getComputerChoice())
   });
 });
+
 
 // give this error: Uncaught TypeError: Failed to execute 'addEventListener' on 'EventTarget': parameter 2 is not of type 'Object'.
 /*
@@ -66,4 +125,14 @@ choiceButtons.forEach((button) => {
 choiceButtons.forEach((button) => {
   button.addEventListener("click", console.log(playRound(button.className, getComputerChoice())
   ));
-});*/ 
+});*/
+
+//Display the running score, and announce a winner of the game once one player reaches 5 points.
+
+//1. keep track of score DONE
+//2. use DOM methods to show the player and computer scores on screen DONE
+//3. once someone's score hits 5, display game results, stop user from being able to play (remove event listeners from the rps buttons), and have a "play again" button appear 
+//4. if user hits the "play again" button, the game resets (add back the event listeners to the rps buttons)
+
+
+// play again
