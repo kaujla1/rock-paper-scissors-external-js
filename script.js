7d4618940850e5b7853271bc93816794a52c4ee3
Kiv Aujla <89060170+kaujla1@users.noreply.github.com>
2023-06-06 11:00:55 -0400

    Add code to show players' choices

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*generate a random choice for the computer*/
 function getComputerChoice() {
-  let computerOptions = ["Rock", "Paper", "Scissors"];
+  let computerOptions = ["rock", "paper", "scissors"];
   let computerChoice = computerOptions[Math.floor(Math.random() * computerOptions.length)];
   return computerChoice;
 }
@@ -14,47 +14,91 @@ const choiceDiv = document.querySelector(".choices");
 /*Create DOM results div*/
 const resultsDiv = document.createElement("div");
 resultsDiv.classList.add("results");
-body.insertBefore(resultsDiv, choiceDiv);  
+body.insertBefore(resultsDiv, choiceDiv);
 
 /*play a single round of rock paper scissors and return the result of the round*/
 let result;
 
+let playerSelectionOuter;
+let computerSelectionOuter;
+
 function playRound(playerSelection, computerSelection) {
+  playerSelectionOuter = playerSelection;
+  computerSelectionOuter = computerSelection;
   if (playerScore.textContent < "5" && computerScore.textContent < "5") { //Makes it so users can't play once someone hits 5 points
-    if (playerSelection === "rock" && computerSelection === "Scissors") {
+    if (playerSelection === "rock" && computerSelection === "scissors") {
       result = "You win! Rock beats Scissors.";
-      resultsDiv.textContent = result; 
-    } else if (playerSelection === "rock" && computerSelection === "Paper") {
+      resultsDiv.textContent = result;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
       result = "You lose. Paper beats Rock.";
       resultsDiv.textContent = result;
-    } else if (playerSelection === "rock" && computerSelection === "Rock") {
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
       result = "It's a tie! You both chose rock.";
       resultsDiv.textContent = result;
-    } else if (playerSelection === "paper" && computerSelection === "Rock") {
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
       result = "You win! Paper beats Rock.";
       resultsDiv.textContent = result;
-    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
       result = "You lose. Scissors beat Paper.";
       resultsDiv.textContent = result;
-    } else if (playerSelection === "paper" && computerSelection === "Paper") {
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
       result = "It's a tie! You both chose Paper.";
       resultsDiv.textContent = result;
-    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
       result = "You win! Scissors beat Paper.";
       resultsDiv.textContent = result;
-    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
       result = "You lose. Rock beats Scissors.";
       resultsDiv.textContent = result;
-    } else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
       result = "It's a tie! You both chose Scissors.";
       resultsDiv.textContent = result;
     }
     keepScore();
+    showPlayerChoice();
+    showComputerChoice();
+  }
+}
+
+/*Connect to image divs*/
+const playerChoiceImgDiv = document.querySelector(".player-choice-img");
+const computerChoiceImgDiv = document.querySelector(".computer-choice-img");
+
+/*Show players' choices*/
+const playerChoiceImg = document.createElement("img");
+playerChoiceImg.src = "";
+
+const computerChoiceImg = document.createElement("img");
+
+
+function showPlayerChoice() {
+  if (playerSelectionOuter === "rock") {
+    playerChoiceImg.src = "./rps-icons/rock.svg";
+    playerChoiceImgDiv.appendChild(playerChoiceImg);
+  } else if (playerSelectionOuter === "paper") {
+    playerChoiceImg.src = "./rps-icons/paper.svg";
+    playerChoiceImgDiv.appendChild(playerChoiceImg);
+  } else if (playerSelectionOuter === "scissors") {
+    playerChoiceImg.src = "./rps-icons/scissors.svg";
+    playerChoiceImgDiv.appendChild(playerChoiceImg);
+  }
+}
+
+function showComputerChoice() {
+  if (computerSelectionOuter === "rock") {
+    computerChoiceImg.src = "./rps-icons/rock.svg";
+    computerChoiceImgDiv.appendChild(computerChoiceImg);
+  } else if (computerSelectionOuter === "paper") {
+    computerChoiceImg.src = "./rps-icons/paper.svg";
+    computerChoiceImgDiv.appendChild(computerChoiceImg);
+  } else if (computerSelectionOuter === "scissors") {
+    computerChoiceImg.src = "./rps-icons/scissors.svg";
+    computerChoiceImgDiv.appendChild(computerChoiceImg);
   }
 }
 
 /*keep track of players' scores after each round*/
-const scoreboardPlayer = document.querySelector(".player"); 
+const scoreboardPlayer = document.querySelector(".player");
 
 const playerScore = document.createElement("p");
 playerScore.classList.add("player-score");
@@ -65,7 +109,7 @@ const scoreboardComputer = document.querySelector(".computer");
 
 const computerScore = document.createElement("p");
 computerScore.classList.add("computer-score")
-computerScore.textContent = 0; 
+computerScore.textContent = 0;
 scoreboardComputer.appendChild(computerScore);
 
 function keepScore() {
@@ -108,13 +152,16 @@ const playAgainButton = document.createElement("button");
 playAgainButton.classList.add("play-again");
 playAgainButton.textContent = "Play Again";
 
+playAgainButton.addEventListener("click", () => {
+  playerScore.textContent = 0;
+  computerScore.textContent = 0;
+  playerChoiceImg.remove();
+  computerChoiceImg.remove();
+  gameResult.textContent = "";
+  playAgainButton.style.visibility = "hidden";
+})
+
 function playAgain() {
   playAgainButton.style.visibility = "visible"; //necessary to have button "reappear" after it's been hidden
   body.appendChild(playAgainButton);
-  playAgainButton.addEventListener("click", () => {
-    playerScore.textContent = 0;
-    computerScore.textContent = 0;
-    playAgainButton.style.visibility = "hidden"; 
-    gameResult.textContent = "";
-  })
-}
+} 
